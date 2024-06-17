910be2ea6bd54aeabd433de3a5a3dab66f0f8a8b
Sanjana Vivek <sanjana@vivekv.info>
2024-06-18 01:01:01 +0530

    Update to CreatePage file

--- src/components/CreatePage.jsx
+++ src/components/CreatePage.jsx
@@ -2,15 +2,36 @@ import { Button } from "antd";
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import "./Navbar.css";
+import "./CreatePage.css";
+import { useState } from "react";
 
 function CreatePage() {
   const navigate = useNavigate();
+  const [file, setFile] = useState();
+  function handleChange(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
   return (
     <>
       <Navbar />
+      <h1 className="heading-1">Upload an image and enter your thoughts</h1>
       <br />
-      <br />
-      
+      <section className="section-1">
+        <h2 className="heading-1"> Add your Image </h2>
+        <br />
+        <input type="file" onChange={handleChange} />
+        <br />
+        <br />
+        <h2 className="heading-1">Collect your thoughts</h2>
+        <textarea rows={20} cols={140}></textarea>
+      </section>
+      <br/>
+      <section className="upload-image-button">
+          <Button onClick={()=>navigate("/")}>Create</Button>
+      </section>
+      <br/>
+      <br/>
     </>
   );
 }
